11f112c6b85f4d90c523970f32ef6d41e8738c5c
Pankaj <54410535+Pankaj1729@users.noreply.github.com>
2022-03-14 11:38:37 +0530

    Update server.java

--- Zoho-Java-Assignment-2022/server.java
+++ Zoho-Java-Assignment-2022/server.java
@@ -1,18 +1,31 @@
-import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class server {
    public static void main(String[] args) {
     try {
-        ServerSocket socket = new ServerSocket(5000);
+        try (ServerSocket serSocket = new ServerSocket(5000)) {
+            System.out.println("Connected!");
 
-        System.out.println("Connected");
 
-        //Take Input from user
+//accept client requests
+            Socket ss = serSocket.accept();
 
-        DataInputStream input = new DataInputStream(System.in);
+            Scanner sc = new Scanner(ss.getInputStream());
+
+            String requestMsg = sc.nextLine();
+
+            String replyMsg = "Hi " + requestMsg; 
+
+            //send msg
+            PrintStream pt = new PrintStream(ss.getOutputStream());
+            pt.println(replyMsg);
+        }
+
 
     } catch (UnknownHostException e) {
         e.getStackTrace();
