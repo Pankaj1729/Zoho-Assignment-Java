096ef32c1b846fc874458715e25c85526ab6a0e3
Pankaj <54410535+Pankaj1729@users.noreply.github.com>
2022-03-14 11:39:02 +0530

    Update client.java

--- Zoho-Java-Assignment-2022/client.java
+++ Zoho-Java-Assignment-2022/client.java
@@ -1,11 +1,35 @@
-import java.io.InputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) {
 
-        Socket socket = new Socket();
+        Scanner sc = new Scanner(System.in);
+        try {
+            try (Socket socket = new Socket("127.0.0.1", 5001)) {
+                Scanner sc1 = new Scanner(socket.getInputStream());
+                System.out.println("Enter your Name: ");
+                String message = sc.nextLine();
 
-        
+                //print on client machine
+                PrintStream ps = new PrintStream(socket.getOutputStream());
+                ps.println(message);
+
+                //server respose 
+
+                String reply = sc1.nextLine();
+                System.out.println(reply);
+                sc.close();
+            }
+
+            //to input on client machine
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
